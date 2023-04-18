9731635833d86d208c4f6ab78baaf7f0279620fb
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2023-04-18 19:45:04 +0200

    reinforce update function

---
+++ models/reinforce.py
@@ -0,0 +1,21 @@
+import torch 
+
+def reinforce_update(returns: torch.Tensor, log_prob_actions: torch.Tensor, optimizer) -> float:
+    """Update policy using REINFORCE algorithm.
+    
+    Args:
+        returns (torch.Tensor): The returns for each timestep.
+        log_prob_actions (torch.Tensor): The log probability of the actions taken.
+        optimizer (torch.optim.Optimizer): The optimizer to use.
+        
+    Returns:
+        float: loss value."""
+    
+    returns = returns.detach()
+    loss = - (returns * log_prob_actions).sum()
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    return loss.item()
